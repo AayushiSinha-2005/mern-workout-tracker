54365a83680f2f32570e2e31d5c7d94ae8f5cb95
AayushiSinha-2005 <jiyasinha2055@gmail.com>
2026-06-11 19:11:42 +0530

    fix cors error for netlify

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const cors = require('cors');
 const express = require('express')
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
@@ -17,13 +18,16 @@ const app = express()
 const PORT = process.env.PORT || 4000
 
 // middleware
-app.use(express.json())
+app.use(cors({
+  origin: "https://workout-buddy-aayushi.netlify.app"
+}));
+
+app.use(express.json());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method)
   next()
 })
-
 // routes
 app.use('/api/workouts', workoutRoutes)
 app.use('/api/user', userRoutes)
